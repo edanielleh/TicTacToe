2422d481d3a3e259024feccae27e76030b12eb09
edanielleh <edanielleh@gmail.com>
2017-10-29 17:18:05 -0600

    Add files via upload

--- tictactoe.js
+++ tictactoe.js
@@ -1,139 +1,128 @@
 $(document).ready(function() {
-    var grid = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I'];
-    var computerGame;
-    var randomGrid;
-    var gamePiece = true;
-    // BOTH start up sets players and starting piece
+    var grid = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I']; //tictactoe grid
+    var computerGame; //set true if one player, false if two player
+    var computerGridChoice; //one player only, random grid choice by computer player
+    var xTurn = true; // false on O turn
+    function disableBoth() {
+        $('#onePlayer').hide();
+        $('#twoPlayer').hide();
+        $("#message1").hide();
+    }
+
+    function createButtonCallsOnePlayer() {
+        $(".main").click(onClickOnePlayer);
+    }
+
+    function createButtonCallsTwoPlayer() {
+        $(".main").click(onClickTwoPlayer);
+    }
+    // begins a ONE PLAYER game
     $("#onePlayer").click(function() {
         computerGame = true;
         console.log("this is a computer game");
         disableBoth();
         createButtonCallsOnePlayer();
     });
+    // begins a TWO PLAYER game
     $("#twoPlayer").click(function() {
         computerGame = false;
         disableBoth();
         createButtonCallsTwoPlayer();
     });
-    // button calls for main FOR two player   
-    function createButtonCallsTwoPlayer() {
-        $(".main").click(onClickTwoPlayer);
-        //gamePiece = true;
-    }
-    // button calls one player 
-    function createButtonCallsOnePlayer() {
-        $(".main").click(onClickOnePlayer);
-    }
-    // BOTH disables one player and two player buttons 
-    function disableBoth() {
-        $('#onePlayer').hide();
-        $('#twoPlayer').hide();
-        $("#message1").hide();
-    }
-    // BOTH switches boolean state for game piece
-    function switchPiece() {
-        gamePiece = !gamePiece;
-        console.log("the game piece is set to " + gamePiece);
-    }
-    // BOTH  
+    // ONE and TWO PLAYER onClick Functions
     function gridSwitch(item, assignment) {
-        console.log(item);
         var index = parseInt(String.fromCharCode(item.charCodeAt(0) - 17));
         grid[index] = assignment;
     }
-    // ONE PLAYER ONLY 
-    function assignMove(randomGrid) {
-        console.log("#" + randomGrid);
-        $("#" + randomGrid).text("O");
-        $("#" + randomGrid).prop("disabled", true);
-        var index = parseInt(String.fromCharCode(randomGrid.charCodeAt(0) - 17));
+
+    function switchPiece() {
+        xTurn = !xTurn;
+    }
+
+    function assignMove(computerGridChoice) { // ONE PLAYER ONLY
+        $("#" + computerGridChoice).text("O");
+        $("#" + computerGridChoice).prop("disabled", true);
+        var index = parseInt(String.fromCharCode(computerGridChoice.charCodeAt(0) - 17));
         grid[index] = "O";
     }
-    // ONE PLAYER ONLY 
-    function getRandom() {
-        randomGrid = grid[Math.floor(Math.random() * grid.length)];
-        console.log("this is the randomGrid choice " + randomGrid);
-        if (randomGrid === "X" || randomGrid === "O") {
+
+    function getRandom() { // ONE PLAYER ONLY
+        computerGridChoice = grid[Math.floor(Math.random() * grid.length)];
+        if (computerGridChoice === "X" || computerGridChoice === "O") {
             getRandom();
         }
-        assignMove(randomGrid);
-    }
-    //BOTH sets X or O in box and calls f to switch pieces then disables used button 
-    function onClickTwoPlayer() {
-        console.log("running on Click Two Player" + gamePiece);
-        if (gamePiece === true) {
-            $(this).text("X");
-            console.log(this.id);
-            gridSwitch(this.id, "X");
-            console.log("this is the current grid after X was called " + grid);
-            winCheck();
-            $(this).prop("disabled", true);
-            switchPiece();
-        } else {
-            $(this).text("O");
-            console.log(this.id);
-            gridSwitch(this.id, "O");
-            console.log("this is the current grid after O was called " + grid);
-            winCheck();
-            $(this).prop("disabled", true);
-            switchPiece();
-        }
+        assignMove(computerGridChoice);
     }
 
-    function onClickOnePlayer() {
-        console.log("running on Click One Player" + gamePiece);
-        if (gamePiece === true) {
-            $(this).text("X");
-            console.log(this.id);
-            gridSwitch(this.id, "X");
-            console.log("this is the current grid after X was called " + grid);
-            $(this).prop("disabled", true);
-            winCheck();
-            getRandom();
-            winCheck();
+        function drawCheck() {
+            for (i = 0; i < grid.length; i++) {
+                if (grid[i] !== "X" && grid[i] !== "O") {
+                    return false;
+                }
+            }
+            return true;
         }
-    }
-    //disables all buttons 
-    function disableAll() {
-        $(".main").prop("disabled", true);
-    }
 
-    function winCheck() {
-        for (i = 0; i < 9; i += 3) {
-            if (grid[i] == grid[i + 1] && grid[i + 1] == grid[i + 2]) {
-                alert("Game is over " + grid[i] + "has won!");
+        function winCheck() {
+            for (i = 0; i < 9; i += 3) {
+                if (grid[i] == grid[i + 1] && grid[i + 1] == grid[i + 2]) {
+                    alert("Game is over " + grid[i] + "has won!");
+                    disableAll();
+                    console.log("Game is Over");
+                }
+            }
+            for (i = 0; i < 3; i++) {
+                if (grid[i] == grid[i + 3] && grid[i + 3] == grid[i + 6]) {
+                    alert("Game is over " + grid[i] + " has won!");
+                    disableAll();
+                    console.log("Game is Over");
+                }
+            }
+            if (grid[0] == grid[4] && grid[4] == grid[8]) {
+                alert("Game is over " + grid[0] + " has won!");
                 disableAll();
                 console.log("Game is Over");
             }
-        }
-        for (i = 0; i < 3; i++) {
-            if (grid[i] == grid[i + 3] && grid[i + 3] == grid[i + 6]) {
-                alert("Game is over " + grid[i] + " has won!");
+            if (grid[2] == grid[4] && grid[4] == grid[6]) {
+                alert("Game is over " + grid[2] + " has won!");
                 disableAll();
                 console.log("Game is Over");
             }
-        }
-        if (grid[0] == grid[4] && grid[4] == grid[8]) {
-            alert("Game is over " + grid[0] + " has won!");
-            disableAll();
-            console.log("Game is Over");
-        }
-        if (grid[2] == grid[4] && grid[4] == grid[6]) {
-            alert("Game is over " + grid[2] + " has won!");
-            disableAll();
-            console.log("Game is Over");
-        }
-        if (drawCheck()) {
-            alert("Draw");
-        }
-    }
-
-    function drawCheck() {
-        for (i = 0; i < grid.length; i++) {
-            if (grid[i] !== "X" && grid[i] !== "O") {
-                return false;
+            if (drawCheck()) {
+                alert("Draw");
             }
         }
-        return true;
-    }
-});
+        //ONE PLAYER sets X or O in box, disables box, creates next turn
+        function onClickOnePlayer() {
+            console.log("running on Click One Player" + xTurn);
+            if (xTurn === true) {
+                $(this).text("X");
+                gridSwitch(this.id, "X");
+                $(this).prop("disabled", true);
+                winCheck();
+                getRandom();
+                winCheck();
+            }
+        }
+        //TWO PLAYER sets X or O in box, disables box, creates next turn
+        function onClickTwoPlayer() {
+            console.log("running on Click Two Player" + xTurn);
+            if (xTurn === true) {
+                $(this).text("X");
+                gridSwitch(this.id, "X");
+                winCheck();
+                $(this).prop("disabled", true);
+                switchPiece();
+            } else {
+                $(this).text("O");
+                gridSwitch(this.id, "O");
+                winCheck();
+                $(this).prop("disabled", true);
+                switchPiece();
+            }
+        }
+        //disables all buttons
+        function disableAll() {
+            $(".main").prop("disabled", true);
+        }
+    });
